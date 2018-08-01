fea5c115642ae298fe1393e16775c7b6cc70e754
Ashish kumar shrivastav <a.shrivastav@globallogic.com>
2018-08-01 12:30:52 +0530

    Using template and bindings

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
-
+  title = "List of courses";
   constructor() { }
 
   ngOnInit() {
   }
 
+  getTitle() {
+    return this.title;
+  }
 }

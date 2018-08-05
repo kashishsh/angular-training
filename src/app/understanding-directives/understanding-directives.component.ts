14ce0e98e2c8eb2a8f98890ef5143eac7c9c2343
Ashish kumar shrivastav <a.shrivastav@globallogic.com>
2018-08-05 22:00:47 +0530

    ngFor and change detection

--- src/app/understanding-directives/understanding-directives.component.ts
+++ src/app/understanding-directives/understanding-directives.component.ts
@@ -6,12 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./understanding-directives.component.css']
 })
 export class UnderstandingDirectivesComponent implements OnInit {
-  courses = [
+  courses: any = [
     { id:1, name: 'Course1'},
     { id:2, name: 'Course2'},
     { id:3, name: 'Course3'},
     { id:4, name: 'Course4'}
   ];
+  addCourse() {
+    this.courses.push({ id: 5, name: 'Course5'});
+  }
+  onRemove(course) {
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
+  onUpdate(course) {
+    course.name = "Update";
+  }
   constructor() { }
 
   ngOnInit() {

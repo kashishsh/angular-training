1dcce5daa75ce5a75ecd24856588fd764bf1abf8
Ashish kumar shrivastav <a.shrivastav@globallogic.com>
2018-08-08 12:46:40 +0530

    Creating data

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -8,8 +8,19 @@ import { Http } from '@angular/http';
 })
 export class PostsComponent implements OnInit {
   posts: any[];
-  constructor(http: Http) {
-    http.get('https://jsonplaceholder.typicode.com/posts')
+  private url = 'https://jsonplaceholder.typicode.com/posts';
+  createPost(input: HTMLInputElement) {
+    let post: any = { title: input.value };
+
+    this.http.post(this.url, JSON.stringify(post))
+      .subscribe(response => {
+        post.id = response.json().id;
+        this.posts.splice(0, 0, post);
+        input.value= "";
+      });
+  }
+  constructor(private http: Http) {
+    http.get(this.url)
     .subscribe(response => {
       this.posts = response.json();
     })
--- src/http-services.md
+++ src/http-services.md
@@ -1,3 +1,5 @@
 - We'll use https://jsonplaceholder.typicode.com/ for fake api calls.
 - We need to import HttpModule in app.module.ts file to use http service and inject http service to constructor of the class which is using http.
+- All methods of http class return an observable.
+
 

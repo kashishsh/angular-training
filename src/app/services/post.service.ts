11124c3a37dc0b7089b3d2aa16d64cc2b396b534
Ashish kumar shrivastav <a.shrivastav@globallogic.com>
2018-08-09 13:12:36 +0530

    Extracting re-usable error handler method

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -12,25 +12,24 @@ export class PostService {
   private url = 'https://jsonplaceholder.typicode.com/posts';
   constructor(private http: Http) { }
   getPosts() {
-    return this.http.get(this.url);
+    return this.http.get(this.url)
+      .catch(this.handleError);
   }
   createPost(post) {
     return this.http.post(this.url, JSON.stringify(post))
-      .catch((error: Response) => {
-        if(error.status === 400)
-          return Observable.throw(new BadRequestError(error.json()));
-
-        return Observable.throw(new AppError(error.json()));
-      });
+      .catch(this.handleError);
   }
   deletePost(id) {
     return this.http.delete(this.url+"/"+id)
-      .catch((error: Response) => {
-        if(error.status === 404)
-          return Observable.throw(new NotFoundError());
+      .catch(this.handleError);
+  }
+  private handleError(error: Response) {
+    if(error.status === 404)
+      return Observable.throw(new NotFoundError());
+    else if(error.status === 400)
+      return Observable.throw(new NotFoundError());
 
-        return Observable.throw(new AppError(error));
-      });
+    return Observable.throw(new AppError(error.json()));
   }
 
 }
